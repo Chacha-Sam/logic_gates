931202162d1a7639bf0a8b168176b49bbe7f4e0a
ChaCha_Sam <chachasam3.14@gmail.com>
2026-03-24 15:51:57 +0530

    Initial Commit

---
+++ Example_Ckt/exampleCkt.v
@@ -0,0 +1,11 @@
+module example(A,B,C,D,E,F,Y);
+// Running Test
+  input A,B,C,D,E,F;
+  output Y;
+  wire t1, t2, t3, t4, Y;
+  not #1 G0(t4,B);
+  nand #1 G1(t1,A,B);
+  and #1 G2(t2,C,t4,D);
+  nor #1 G3(t3,E,F);
+  nand #1 G4(Y,t1,t2,t3);
+endmodule
